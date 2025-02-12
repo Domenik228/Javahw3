056f12d7d7a46e6874ff78de70d360ca8c76ec5f
Domenik228 <akbarbrawlstarsnick@gmail.com>
2025-02-12 16:24:49 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,31 @@
+
+
+let numbers = alert("Sizda hozir random sonlar chiqishni boshlaydi");
+
+
+let random = Math.round(Math.random() * 100);
+
+alert(`Random orqali chiqqan son: ${random} `);
+
+let son = alert("biz random sonni 2ga kopaytirmoqchi va bo'lmoqchimiz ! ")
+
+let javob = alert(`Random orqali chiqqan son: ${random} edi \nbiz uni 2 ga ko'paytirib ${random * 2} sonini chiqardik \nva shu sonni 2ga bo'lib: ${random / 2} sonini chiqardik \nva shu sonni 3ga bo'lgandagi qoldiq: ${random % 3} sonini chiqardik `);
+
+
+
+
+let number = alert("Sizda hozir random sonlar chiqishni boshlaydi");
+
+
+let randomnumber = Math.round(Math.random() * 100);
+
+alert(`Random orqali chiqqan son: ${randomnumber} `);
+
+let bolish = alert("biz random sonni 2ga kopaytirmoqchi va bo'lmoqchimiz ! ")
+
+let answer = alert(`Random orqali chiqqan son: ${randomnumber} edi \nbiz uni 2 ga ko'paytirib ${randomnumber * 2} sonini chiqardik \nva shu sonni 2ga bo'lib: ${randomnumber / 2} sonini chiqardik \nva shu sonni 3ga bo'lgandagi qoldiq: ${randomnumber % 3} sonini chiqardik `);
+
+
+
+
+
